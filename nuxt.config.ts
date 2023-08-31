51cfb8f12beeab35b07ea2f1df246640dfcafd42
JS <you@example.com>
2023-08-31 15:09:06 +0900

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
   css: [
     "assets/css/tailwind.css",
     "primevue/resources/themes/lara-light-blue/theme.css",
